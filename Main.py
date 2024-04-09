546185b650034aaa832c92d6a203475df1f4787d
narek_asa <narek604@gmail.com>
2024-04-08 18:54:19 -0700

    Updated to select theme from themes.json

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import ttkbootstrap as tb
+from ttkbootstrap import Style
 from ttkbootstrap.constants import *
 import gspread
 # Assuming login_window, register_window, and launcher have been similarly updated to use ttkbootstrap
@@ -8,6 +9,7 @@ from launcher import Launcher
 
 
 class Main(tb.Window):
+
     def register_pressed(self, account_wks):
         register = register_window()
         register.page(self, account_wks)
@@ -19,8 +21,11 @@ class Main(tb.Window):
     def login_pressed(self, account_wks):
         Launcher((1400, 900))
 
-    def __init__(self, themename="techwiz", *args, **kwargs):
-        super().__init__(themename=themename, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        style = Style(theme="techwiz")
+        Style.load_user_themes(style, "themes.json")
 
         sa = gspread.service_account(filename="database_key.json")
         account_sheet = sa.open("accountTest")
