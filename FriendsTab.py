4cef342cb1b551037571b39870489dd2c523855f
CastSo <SC>
2024-04-10 21:03:51 -0700

    initialized FriendsTab

---
+++ FriendsTab.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from ttkbootstrap import Label
+import ttkbootstrap as tb
+from ttkbootstrap.constants import *
+from PIL import ImageTk, Image 
+from ttkbootstrap import Style
+
+class FriendsTab(tb.Frame):
+    def __init__(self,parent):
+        super().__init__(parent)
+        self.parent = parent
