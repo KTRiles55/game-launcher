9e3f6613058badd74288fbb753c23b7e01123776
Kenny <kennymunster5@gmail.com>
2024-04-10 19:42:40 -0700

    Added Store UI to Store tab

--- register_window.py
+++ register_window.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import ttkbootstrap as tb
 from ttkbootstrap.constants import *
 from ctypes import windll, byref, sizeof, c_int
@@ -28,10 +29,33 @@ class register_window(tb.Toplevel):
         COLOR = 0x5e3d49
         windll.dwmapi.DwmSetWindowAttribute(HWND, DWMWA_ATTRIBUTE, byref(c_int(COLOR)), sizeof(c_int))
 
-    def check(self):
-        self.parent.run_login()
+    def check(self, name, password, email, controller):
+        #Logic for checking if info exist
+        new_acc = account(name, password, email)
+        warningLbl = ""
+        wks = controller.accessAccountData()         
 
-    def page(self):
+        # Create new account if it does not exist
+        if ((new_acc.findUsername(wks) == None) and (new_acc.findPassword(wks) == None) and (new_acc.findEmail(wks) == None)):
+            new_acc.create_newuser(wks)
+            confirmLbl = tk.Label(self, text="You have successfully created a new account!\nPlease immediately check your email inbox to verify this account.")
+            confirmLbl.place(anchor='center', relx=1, rely=0)
+            returntoLogBtn = tk.Button(self, text="Return to Login", bg="#888a86", activebackground="#a8aba6", command = lambda: [controller.run_login(), self.destroy()])
+            returntoLogBtn.place(anchor='center', relx=1, rely=1)
+    
+        elif (new_acc.findUsername(wks) != None):
+            warningLbl = tk.Label(self, text="This account name is already used!")
+    
+        elif (new_acc.findPassword(wks) != None):
+            warningLbl = tk.Label(self, text="This password is already used!")
+    
+        elif (new_acc.findEmail(wks) != None):
+            warningLbl = tk.Label(self, text="This email is already used!")
+            
+        #self.send_confirmation_email(email.get())  # Send confirmation email
+        controller.run_login()
+
+    def page(self, controller):
         loginTitle = tb.Label(self, text="Register")
         loginTitle.pack(pady=10)
         loginTitle.config(font=("Courier", 20))
@@ -51,8 +75,8 @@ class register_window(tb.Toplevel):
         nameEn = tb.Entry(entryFrame)
         nameEn.grid(row=1, column=1)
 
-        self.email_entry = tb.Entry(entryFrame)  # Define email_entry
-        self.email_entry.grid(row=2, column=1)
+        email_entry = tb.Entry(entryFrame)  # Define email_entry
+        email_entry.grid(row=2, column=1)
 
         passwordEn = tb.Entry(entryFrame, show="*")
         passwordEn.grid(row=3, column=1)
@@ -60,7 +84,5 @@ class register_window(tb.Toplevel):
         backBtn = tb.Button(entryFrame, text="Back", command = lambda: [self.parent.run_login(), self.withdraw()])
         backBtn.grid(row=4, column=0, pady=10)
 
-        submitBtn = tb.Button(entryFrame, text="Create an Account", command = lambda: [self.check(), self.withdraw()])
+        submitBtn = tb.Button(entryFrame, text="Create an Account", command = lambda: [self.check(nameEn, passwordEn, email_entry, controller), self.withdraw()])
         submitBtn.grid(row=4, column=1, padx=5, pady=10)
-
-
