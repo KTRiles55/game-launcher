c52997ce4699748bdb12efa7930a9149b39b05ca
CastSo <SC>
2024-04-10 21:02:02 -0700

    rearranged tabs

--- menu.py
+++ menu.py
@@ -6,6 +6,9 @@ from ttkbootstrap import Style
 import launcher
 from ttkbootstrap.scrolled import ScrolledFrame
 from store_tab import *
+from FriendsTab import *
+from LibraryTab import *
+from SettingsTab import *
 from PIL import ImageTk, Image
 
 
@@ -25,37 +28,21 @@ class Menu(tb.Frame):
         notebook = tb.Notebook(self, style="custom.TNotebook")
 
         # Tabs
-        home_tab = HomeTab(notebook)
+        friends_tab = FriendsTab(notebook)
         library_tab = LibraryTab(notebook)
         store_tab = StoreTab(notebook)
         settings_tab = SettingsTab(notebook)
 
         # Adding Tabs
-        notebook.add(home_tab, text="Home")
-        notebook.add(store_tab, text="Store")
         notebook.add(library_tab, text="Library")
+        notebook.add(store_tab, text="Store")
+        notebook.add(friends_tab, text="Friends")
         notebook.add(settings_tab, text="Settings")
 
         notebook.pack(expand=True, fill=BOTH)
 
 
-class HomeTab(tb.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.parent = parent
 
-
-class LibraryTab(tb.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.parent = parent
-
-
-
-class SettingsTab(tb.Frame):
-    def __init__(self,parent):
-        super().__init__(parent)
-        self.parent = parent
 
 
 
