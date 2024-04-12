dbd47e9128f5b75aa8e8b3d524a44d5b996eb704
Kenny <kennymunster5@gmail.com>
2024-04-12 15:35:21 -0700

    fixed register page bug

--- register_window.py
+++ register_window.py
@@ -1,23 +1,20 @@
 import tkinter as tk
 import ttkbootstrap as tb
 from ttkbootstrap.constants import *
+from ctypes import windll, byref, sizeof, c_int
 import gspread
 import re
 from account import *
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-try:
-    from ctypes import windll, byref, sizeof, c_int
-except:
-    pass
 
 
 class register_window(tb.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.account = account()
+        self.warningLbl = ""
 
         # Window size and title
         self.geometry("500x600")
@@ -27,41 +24,43 @@ class register_window(tb.Toplevel):
         self.title("")
 
         # Window title color
-        try:
-            HWND = windll.user32.GetParent(self.winfo_id())
-            DWMWA_ATTRIBUTE = 35
-            COLOR = 0x201f1e
-            windll.dwmapi.DwmSetWindowAttribute(HWND, DWMWA_ATTRIBUTE, byref(c_int(COLOR)), sizeof(c_int))
-        except:
-            pass
+        HWND = windll.user32.GetParent(self.winfo_id())
+        DWMWA_ATTRIBUTE = 35
+        COLOR = 0x5e3d49
+        windll.dwmapi.DwmSetWindowAttribute(HWND, DWMWA_ATTRIBUTE, byref(c_int(COLOR)), sizeof(c_int))
 
-    def check(self, name, password, email, controller):
+    def check(self, name, password, email):  
         #Logic for checking if info exist
         new_acc = account(name, password, email)
-        warningLbl = ""
-        wks = controller.accessAccountData()         
+        wks = self.parent.accessAccountData()    
+        
+        if (self.warningLbl != ""):
+            self.warningLbl.destroy()
 
         # Create new account if it does not exist
         if ((new_acc.findUsername(wks) == None) and (new_acc.findPassword(wks) == None) and (new_acc.findEmail(wks) == None)):
             new_acc.create_newuser(wks)
             confirmLbl = tk.Label(self, text="You have successfully created a new account!\nPlease immediately check your email inbox to verify this account.")
-            confirmLbl.place(anchor='center', relx=1, rely=0)
-            returntoLogBtn = tk.Button(self, text="Return to Login", bg="#888a86", activebackground="#a8aba6", command = lambda: [controller.run_login(), self.destroy()])
-            returntoLogBtn.place(anchor='center', relx=1, rely=1)
+            confirmLbl.pack()
+            returntoLogBtn = tk.Button(self, text="Return to Login", bg="#888a86", activebackground="#a8aba6", command = lambda: [self.parent.run_login(), self.destroy()])
+            returntoLogBtn.pack()
+            #self.send_confirmation_email(email.get())  # Send confirmation email 
+            #self.parent.run_login()
     
         elif (new_acc.findUsername(wks) != None):
-            warningLbl = tk.Label(self, text="This account name is already used!")
+            self.warningLbl = tk.Label(self, text="This username is already used!")
+            self.warningLbl.pack()
     
         elif (new_acc.findPassword(wks) != None):
-            warningLbl = tk.Label(self, text="This password is already used!")
+            self.warningLbl = tk.Label(self, text="This password is already used!")
+            self.warningLbl.pack()
     
         elif (new_acc.findEmail(wks) != None):
-            warningLbl = tk.Label(self, text="This email is already used!")
+            self.warningLbl = tk.Label(self, text="This email is already used!")
+            self.warningLbl.pack()
             
-        #self.send_confirmation_email(email.get())  # Send confirmation email
-        controller.run_login()
 
-    def page(self, controller):
+    def page(self):
         loginTitle = tb.Label(self, text="Register")
         loginTitle.pack(pady=10)
         loginTitle.config(font=("Courier", 20))
@@ -90,5 +89,5 @@ class register_window(tb.Toplevel):
         backBtn = tb.Button(entryFrame, text="Back", command = lambda: [self.parent.run_login(), self.withdraw()])
         backBtn.grid(row=4, column=0, pady=10)
 
-        submitBtn = tb.Button(entryFrame, text="Create an Account", command = lambda: [self.check(nameEn, passwordEn, email_entry, controller), self.withdraw()])
+        submitBtn = tb.Button(entryFrame, text="Create an Account", command = lambda: self.check(nameEn, passwordEn, email_entry))
         submitBtn.grid(row=4, column=1, padx=5, pady=10)
