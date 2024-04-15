d2b45de0fe20eecfa771df3d833e1128ae4abf5d
Kenny <kennymunster5@gmail.com>
2024-04-14 22:06:05 -0700

    added some finishing touches

--- register_window.py
+++ register_window.py
@@ -1,10 +1,7 @@
 import tkinter as tk
 import ttkbootstrap as tb
 from ttkbootstrap.constants import *
-try:
-    from ctypes import windll, byref, sizeof, c_int
-except:
-    pass
+from ctypes import windll, byref, sizeof, c_int
 import gspread
 import re
 from account import *
@@ -27,13 +24,10 @@ class register_window(tb.Toplevel):
         self.title("")
 
         # Window title color
-        try:
-            HWND = windll.user32.GetParent(self.winfo_id())
-            DWMWA_ATTRIBUTE = 35
-            COLOR = 0x5e3d49
-            windll.dwmapi.DwmSetWindowAttribute(HWND, DWMWA_ATTRIBUTE, byref(c_int(COLOR)), sizeof(c_int))
-        except:
-            pass
+        HWND = windll.user32.GetParent(self.winfo_id())
+        DWMWA_ATTRIBUTE = 35
+        COLOR = 0x5e3d49
+        windll.dwmapi.DwmSetWindowAttribute(HWND, DWMWA_ATTRIBUTE, byref(c_int(COLOR)), sizeof(c_int))
 
     def check(self, name, password, email):  
         #Logic for checking if info exist
@@ -43,32 +37,39 @@ class register_window(tb.Toplevel):
         if (self.warningLbl != ""):
             self.warningLbl.destroy()
 
-        # Create new account if it does not exist
-        if ((new_acc.findUsername(wks) == None) and (new_acc.findPassword(wks) == None) and (new_acc.findEmail(wks) == None)):
-            new_acc.create_newuser(wks)
-            confirmLbl = tk.Label(self, text="You have successfully created a new account!\nPlease immediately check your email inbox to verify this account.")
-            confirmLbl.pack()
-            returntoLogBtn = tk.Button(self, text="Return to Login", bg="#888a86", activebackground="#a8aba6", command = lambda: [self.parent.run_login(), self.destroy()])
-            returntoLogBtn.pack()
-            #self.send_confirmation_email(email.get())  # Send confirmation email 
-            #self.parent.run_login()
+        # Determines valid account information
+        if (new_acc.is_valid(name, email, password) == False):
+            
+            self.warningLbl = tk.Label(self, text="* New account credentials are invalid. *\n* Please re-enter user information/fill in empty fields. *")
+            self.warningLbl.pack()   
+
+        elif (new_acc.is_valid(name, email, password) == True):
+            # Create new account if it does not exist
+            if ((new_acc.findUsername(wks) == None) and (new_acc.findPassword(wks) == None) and (new_acc.findEmail(wks) == None)):
+                new_acc.create_newuser(wks)
+                confirmLbl = tk.Label(self, text="You have successfully created a new account!\nPlease immediately check your email inbox to verify this account.")
+                confirmLbl.pack()
+                returntoLogBtn = tk.Button(self, text="Return to Login", bg="#888a86", activebackground="#a8aba6", command = lambda: [self.parent.run_login(), self.destroy()])
+                returntoLogBtn.pack()
+                #self.send_confirmation_email(email.get())  # Send confirmation email 
+                #self.parent.run_login()
     
-        elif (new_acc.findUsername(wks) != None):
-            self.warningLbl = tk.Label(self, text="This username is already used!")
-            self.warningLbl.pack()
+            elif (new_acc.findUsername(wks) != None):
+                self.warningLbl = tk.Label(self, text="* This username is already used! *")
+                self.warningLbl.pack()
+       
+            elif (new_acc.findPassword(wks) != None):
+                self.warningLbl = tk.Label(self, text="* This password is already used! *")
+                self.warningLbl.pack()
     
-        elif (new_acc.findPassword(wks) != None):
-            self.warningLbl = tk.Label(self, text="This password is already used!")
-            self.warningLbl.pack()
-    
-        elif (new_acc.findEmail(wks) != None):
-            self.warningLbl = tk.Label(self, text="This email is already used!")
-            self.warningLbl.pack()
+            elif (new_acc.findEmail(wks) != None):
+                self.warningLbl = tk.Label(self, text="* This email is already used! *")
+                self.warningLbl.pack()
             
 
     def page(self):
         loginTitle = tb.Label(self, text="Register")
-        loginTitle.pack(pady=10)
+        loginTitle.pack(pady=10)  
         loginTitle.config(font=("Courier", 20))
 
         entryFrame = tb.Frame(self, borderwidth=10, relief="groove")
