0a0ccf2a35df3ad6bd1a3c5251e362c93d3939bd
Kenny <kennymunster5@gmail.com>
2024-04-30 14:58:57 -0700

    changed string retrieval

--- user.py
+++ user.py
@@ -1,10 +1,12 @@
+from re import T
 import openpyxl 
+import ttkbootstrap as tb
 
 
 class user():
     def __init__(self, username):
-        self.path = "database_offline.xlsx"
-        self.wb_obj = openpyxl.load_workbook(self.path)
+        path = "database_offline.xlsx"
+        self.wb_obj = openpyxl.load_workbook(path)
         self.wks_account = self.wb_obj["accountInfo"]
         self.wks_store = self.wb_obj["store"]
 
@@ -21,7 +23,7 @@ class user():
         row_count = self.wks_account.max_row
         for i in range(1, row_count + 1):
             name = self.wks_account.cell(i, 1).value
-            if name == self.username.get():
+            if name == self.username:
                 return i
             if name is None and empty_row is None:
                 empty_row = i
@@ -29,7 +31,7 @@ class user():
         if empty_row is None:
             empty_row = row_count + 1
 
-        self.wks_account.cell(empty_row, 1).value = self.username.get()
+        self.wks_account.cell(empty_row, 1).value = self.username
         self.wb_obj.save(self.path)
 
         return empty_row
@@ -39,7 +41,8 @@ class user():
             returns:
             username (string)
         """
-        return self.username.get()
+        
+        return self.username
 
     def get_password(self):
         """
@@ -64,17 +67,10 @@ class user():
             returns:
             titles of games belonging to the user
         """
-        # Reload the workbook each time to ensure it's up to date
-        wb_obj = openpyxl.load_workbook(self.path, data_only=True)
-        wks_account = wb_obj["accountInfo"]
-
-        # Use find_row to get the user's row
-        user_row = self.find_row()
-        if user_row is not None:
-            library_cell = wks_account.cell(user_row, 4).value
-            if library_cell is not None:
-                parsed_library = library_cell.split("/")
-                return parsed_library
+        library = self.wks_account.cell(self.find_row(),4).value
+        if(library != None):
+            parsed_library = library.split("/")
+            return parsed_library
         return []
 
     def update_library(self, new):
@@ -90,8 +86,6 @@ class user():
         else:
             self.wks_account.cell(self.find_row(),4).value = new
         self.wb_obj.save("database_offline.xlsx")
-
-
  
     
     def get_game_details(self):
@@ -119,22 +113,50 @@ class user():
                     library[count]["Release_Date"] = self.wks_store.cell(i+1, 6).value
                     count += 1
         return library
+    
+    
+    def getParsedFriendsList(self):
+        # Get friends list associated with user account
+        friendsList = self.wks_account.cell(self.find_row(), 5).value
+        if (friendsList != None):
+            parsedFriendsList = friendsList.split(">>")
+            return parsedFriendsList
+        return []
+    
+    def updateFriendsList(self, newFriend):
+        # Add new friend to account data
+        friendsList = self.wks_account.cell(self.find_row(), 5).value
+        if (friendsList != None):
+            self.wks_account.cell(self.find_row(), 5).value += '>>' + newFriend
+        else:
+            self.wks_account.cell(self.find_row(), 5).value = newFriend
+        
+        self.wb_obj.save("database_offline.xlsx")
+    
+   
+    def sendFriendRequest(self, friendUsername):
+        # sends friend request and adds it into the other user's database
+        friendVar = tb.StringVar(value=friendUsername)
+        friend = user(friendVar.get())
+        friendIdLoc = self.wks_account.cell(friend.find_row(), 1).row
+        message = "{profile} has sent you a friend request.".format(profile = self.get_username())
+        friendRequests = self.wks_account.cell(friendIdLoc, 6).value 
+        if (friendRequests != None):
+            self.wks_account.cell(friendIdLoc, 6).value += ">>" + message
+        else:
+            self.wks_account.cell(friendIdLoc, 6).value = message
+        
+        self.wb_obj.save("database_offline.xlsx")
 
 
-    def check_inlibrary(self, game):
-        """
-        Using game title checks if title is in library
-        args:
-            game (string)
-        returns:
-            status (string)
+    def listFriendRequests(self):
+        # display friend requests awaiting response
+        friendRequests = self.wks_account.cell(self.find_row(), 6).value
+        if (friendRequests != None):
+            parseFriendRequests = friendRequests.split(">>")
+            return parseFriendRequests
+        
+        return []
+                 
 
-        """
-        status = False
-        titles = self.get_parsed_library()
-        for title in titles:
-            if(title == game):
-                status = True
-                break
-        return status
-            
+    
